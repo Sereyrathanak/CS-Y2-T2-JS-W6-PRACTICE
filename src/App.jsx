f62d30b80fbc6a45d3735c32bdac7df7111f0184
Sereyrathanak <Sereyrathanak.rith@student.cadt.edu.kh>
2026-02-26 14:58:20 +0700

    complete step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,15 @@
-import pnLogo from "./assets/pn-logo.png";
+
+import Header from "./components/Header";
+import Score from "./components/Score";
+import { HTML_RESULTS } from "./data";
 
 function App() {
   return (
     <>
-      <header id="header">
-        <img src={pnLogo} alt="PN Logo" />
-        <h1>Students results for (fake batch name)</h1>
-      </header>
+      <Header batchName="Batch C2-S1" />
 
       <main className="scores-container">
-        <div class="scores">
-          <h1>Fake Course</h1>
-
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <Score courseName="HTML" courseResults={HTML_RESULTS} />
       </main>
     </>
   );
